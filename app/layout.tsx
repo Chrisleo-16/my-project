102d59f8141a104689fe432799b3f76ff45a4bec
Chrisleo-16 <152534621+Chrisleo-16@users.noreply.github.com>
2024-11-27 08:59:51 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider frontendApi={process.env.NEXT_PUBLIC_CLERK_FRONTEND_API}>
       <html lang="en" suppressHydrationWarning>
         <body className="scroll-smooth">
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
