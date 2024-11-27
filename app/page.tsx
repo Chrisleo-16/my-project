3f55eb22d7b23540e29cb700d239af0ae6049b32
Chrisleo-16 <152534621+Chrisleo-16@users.noreply.github.com>
2024-11-27 09:24:28 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,11 @@ import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 import Testimonial from "@/components/Testimonial";
 import Trusted from "@/components/Trusted";
-import Head from "next/head";
+
 
 export default function Home() {
   return (
     <>
-      <Head>
-                <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                <link rel="manifest" href="/site.webmanifest" />
-            </Head>
       <Hero />
       <Trusted />
       <About />
